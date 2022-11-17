633c33a874e83bc7adc8659dfe2f042e08feb844
AndreaDotta <117829484+S5475373@users.noreply.github.com>
2022-11-17 19:47:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    float a=0, b=0;
+    int opzione=0;
+
+    cout << "inserire il valore di a" << endl;
+    cin >> a;
+
+    cout << "inserire il valore di b" << endl;
+    cin >> b;
+
+    cout << "Scegliere quale area calcolare" << endl << "Digita 0 per l'area del triangolo" << endl << "Digita 1 per l'area del quadrato" << endl << "Digita 2 per l'area del rettangolo" << endl;
+    cin >> opzione;
+
+    switch (opzione) {
+        case (0):
+            cout << ((a*b)/2);
+            break;
+        case (1):
+            cout << (a*a);
+            break;
+        case (2):
+            cout << (a*b);
+            break;
+        default:
+            cout << "operazione non valida" << endl;
+    }
+    return 0;
 }
